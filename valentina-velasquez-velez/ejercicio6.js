44d78f8efb1cbad5e4df86a32269ce647f913975
Valentina Velasquez Velez <valentina.velasquez@globant.com>
2026-01-24 11:34:06 -0500

    feat: implementación de lógica para impuestos de ventas

---
+++ valentina-velasquez-velez/ejercicio6.js
@@ -0,0 +1,22 @@
+/***Impuestos de Ventas:** Recibes un arreglo de montos de ventas brutas. 
+ * Calcula el IVA (19%) de cada una. Si una venta supera los $500.000,
+ *  aplica una retención del 4% adicional sobre el valor neto. Retorna el
+ *  acumulado total de impuestos.*/
+
+function calcularImpuestosVentas(ventas) {
+  let totalImpuestos = 0
+    const IVA = 0.19
+    const retencion = 0.04
+
+    for (let i = 0; i < ventas.length; i++) {
+        let montoVenta = ventas[i]
+        let impuestoIVA = montoVenta * IVA
+        let impuestoRetencion = 0       
+        if (montoVenta > 500000) {
+            impuestoRetencion = montoVenta * retencion
+        }
+        totalImpuestos += impuestoIVA + impuestoRetencion
+    }
+    return totalImpuestos
+}
+module.exports = calcularImpuestosVentas
