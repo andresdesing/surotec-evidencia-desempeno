46c6e67a06f0751ea6c9d49a1967ad0acfaf181e
Tban Moreno <esteban.moreno.quintero@gmail.com>
2026-01-24 11:15:19 -0500

    feat: actualización de precios dinámica y visualización en tabla - ejercicio 2

--- johann-esteban-moreno-quintero/menu.js
+++ johann-esteban-moreno-quintero/menu.js
@@ -1,4 +1,5 @@
 import { calcularDenominacionesBilletes } from './ejercicio1.js';
+import { actualizarPreciosInventario } from './ejercicio2.js';
 
 function menuPrincipal() {
     let continuar = true;
@@ -7,7 +8,8 @@ function menuPrincipal() {
         let opcion = prompt(
             "--- TALLER DE LÓGICA DE PROGRAMACIÓN ---\n" +
             "1. Cajero de Denominaciones\n" +
-            "0. Salir\n\n" + 
+            "2. Actualizar precios\n" +
+            "0. Salir\n" +
             "Seleccione una opción:"
         );
 
@@ -25,10 +27,34 @@ function menuPrincipal() {
                     alert("Resultado:\n" + JSON.stringify(resultado1, null, 2));
                 }
                 break;
-            default:
-                alert("Opción no válida.");
+           
+
+            case '2':
+
+                let nombre = prompt("Ingrese el nombre del producto:", "Laptop");
+                let stock = parseInt(prompt(`Ingrese el stock para ${nombre}:`, "3"));
+                let precio = parseFloat(prompt(`Ingrese el precio para ${nombre}:`, "1000000"));
+
+                const productosUsuario = [{ nombre, stock, precio }];
+
+                console.log("Datos originales:", productosUsuario);
+
+                const resultado2 = actualizarPreciosInventario(productosUsuario);
+
+                alert(
+                    `Resultado para ${nombre}:\n` +
+                    `Stock: ${stock}\n` +
+                    `Precio Original: ${precio}\n` +
+                    `Precio Final (ajustado): ${resultado2[0].precio}`
+                );
+
+                console.table(resultado2);
+                break;
+                default: alert("Opción no válida.");
                 break;
         }
+
+
     }
 }
 menuPrincipal();
