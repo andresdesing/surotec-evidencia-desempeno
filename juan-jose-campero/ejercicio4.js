60139d1ed461b8e4f3f23aba41d32a214f6cae6b
Hoshieko <Jujocamcas22@gmail.com>
2026-01-24 12:00:59 -0500

    Feat: Implementacion logica para la aplicacion de descuento del carrito

---
+++ juan-jose-campero/ejercicio4.js
@@ -0,0 +1,19 @@
+descuentoCarrito(productos) {
+    const cantidadDigital = productos.filter(p => p.tipo === 'digital').length;
+
+    const aplicarDescuento = cantidadDigital >= 3;
+
+    let total = 0;
+    for (const producto of productos) {
+        let precioFinal = producto.precio;
+
+        if (aplicarDescuento && producto.tipo === 'digital') {
+            precioFinal *= 0.8; 
+        }
+        total += precioFinal;
+    }
+
+    return total;
+}
+
+export default descuentoCarrito;
