5fd59d1d72e40472ef7be5bdbc6316a9fcbb26c4
Samuel01112105 <samuelalvarezmontoya1@gmail.com>
2026-01-24 10:40:25 -0500

    feat: implementación de lógica para el ejercicio 11 (Capacidad de aula - cantidad de estudiantes que quedan fuera)

---
+++ samuel-alvarez-montoya/ejercicio11.js
@@ -0,0 +1,16 @@
+function calcularEstudiantesFuera(capacidadMaxima, gruposEstudiantes) {
+    
+    let capacidadRestante = capacidadMaxima;
+    let estudiantesFuera = 0;
+
+    for (let i = 0; i < gruposEstudiantes.length; i++) {
+        if (gruposEstudiantes[i] <= capacidadRestante) {
+            capacidadRestante -= gruposEstudiantes[i];
+        } else {
+            estudiantesFuera = gruposEstudiantes[i];
+            break;
+        }
+    }
+
+    return estudiantesFuera;
+}
