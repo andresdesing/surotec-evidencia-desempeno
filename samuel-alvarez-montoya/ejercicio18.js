47f5e0069f80bb6cd8c6d39c6767b36bb6bb8fc1
Samuel01112105 <samuelalvarezmontoya1@gmail.com>
2026-01-24 11:13:02 -0500

    feat: implementación de lógica para el ejercicio 18 (Restricción de biblioteca - 10 días de retraso o multa > 10000)

---
+++ samuel-alvarez-montoya/ejercicio18.js
@@ -0,0 +1,18 @@
+function validarPrestamoBiblioteca(prestamos) {
+    
+    let multaTotal = 0;
+
+    for (let i = 0; i < prestamos.length; i++) {
+        if (prestamos[i].diasRetraso > 10) {
+            return "Prestamo Denegado";
+        }
+
+        multaTotal += prestamos[i].multa;
+
+        if (multaTotal > 10000) {
+            return "Prestamo Denegado";
+        }
+    }
+
+    return "Prestamo Aprobado";
+}
